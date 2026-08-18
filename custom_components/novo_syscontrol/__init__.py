0ff57e045ebfe737aa4464528564b220d57f5d85
matthiasgallant-ctrl <matthias.gallant@deltagroep.be>
2026-08-18 16:03:52 +0200

    Fix async_setup_entry by adding return True
    Add return statement to async_setup_entry function.

--- custom_components/novo_syscontrol/__init__.py
+++ custom_components/novo_syscontrol/__init__.py
@@ -65,6 +65,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
+    return True
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
